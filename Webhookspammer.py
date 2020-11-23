a533e153741f0abfcf73e7364dd3f53944c8b27c
sexyman2343 <74887244+sexyman2343@users.noreply.github.com>
2020-11-22 20:49:08 -0600

    Add files via upload

---
+++ Webhookspammer.py
@@ -0,0 +1,21 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import requests
+import json
+import sys
+wburl = sys.argv[1]
+webhook = DiscordWebhook(url=wburl)
+
+count = 0
+while True:
+    r = requests.get("https://some-random-api.ml/img/cat")
+    rr = r.json()
+    image = rr["link"]
+    nigger = count + 1 
+    webhook = DiscordWebhook(url=wburl)
+    
+    embed = DiscordEmbed(title='Le Nice Token Logger', description='Enjoy the Pussy Matey', color=0xff0000)
+    embed.set_image(url=image)
+    webhook.add_embed(embed)
+    
+    webhook.execute()
+    print(f"Thy webhook has been spammed", count + 1)
