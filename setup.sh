115d2a4b78eb7c919457f214391f76abdb3bba07
sexyman2343 <74887244+sexyman2343@users.noreply.github.com>
2020-11-23 01:34:46 -0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+echo Please Allow Up To 2 Minutes....
+
+
+apt update
+sleep 2
+apt upgrade
+sleep 2
+apt install python3 
+sleep 2
+apt install python3-pip
+sleep 2
+pip3 install -r /root/sexmaster/requirements.tx
+
+cd sexmaster
+python3 setup.py
